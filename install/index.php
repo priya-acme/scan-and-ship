7e58d3e70b65aab0bdd4f8abc338eb7e3642178c
priya mehrotra <priya@acmeintech.in>
2017-06-17 15:09:16 +0530

    scan-n-ship

--- install/index.php
+++ install/index.php
@@ -6,60 +6,68 @@ include '../includes/utils/Shopify.php';
 $Shopify = new Shopify();
 $Stores = new Stores();
 $shop = $_REQUEST['shop'];
+
 $code = isset($_GET["code"]) ? $_GET["code"] : false;
 
 if ($shop && !$code) {
-    // validate the shopify url
-    if (!$Shopify->validateMyShopifyName($shop)) {
-        echo "Invalid shopify url";
-    }
-    
-    $redirect_url = $Shopify->getAuthUrl($shop);
-    header("Location: $redirect_url");
-    
+	// validate the shopify url
+	if (!$Shopify->validateMyShopifyName($shop)) {
+		echo "Invalid shopify url";
+	}
+	$redirect_url = $Shopify->getAuthUrl($shop);
+	//echo 'redirect url : '.$redirect_url;
+	header("Location: $redirect_url");
+	
 }
 
 if ($code) {
-    echo $code;
-    // we want to exchange the temp token passed by the shopify server during the installation process
-    // in exchange of a permanent token which we need in order to get/gain access on the shopify store
-    $exchange_token_response = $Shopify->exchangeTempTokenForPermanentToken($shop, $code);
-    
-    // validate access token
-    if(!isset($exchange_token_response->access_token) && isset($exchange_token_response->errors)) {
-        // access token is not valid, redirect user to error page
-        echo "<pre>";
-        print_r($exchange_token_response->errors);
-        echo "</pre>";
-    }
-    
-    $access_token = $exchange_token_response->access_token;
-    
-    // we check if it's a fresh installation
-    $shop_info = $Stores->is_shop_exists($shop);
-    
-    if (empty($shop_info)) {
-        //echo "okkkk";// this means that's it's a fresh installation, so we do the installation process
-        $Stores->addData(array(
-            "store_url" => $shop,
-            "access_key" => SHOPIFY_API_KEY,
-            "token" => $access_token,
-            "created_at" => date("Y-m-d")
-        ));
-    } else {
-        //echo 'byeee';
-        $Stores->updateData(array(
-            "token" => $access_token,
-            "access_key" => SHOPIFY_API_KEY,
-            "modified_at" => date("Y-m-d")
-        ), "store_url = '$shop'");
-    }
-    
-    header("Location: " . APP_URL);
+	
+	// we want to exchange the temp token passed by the shopify server during the installation process
+	// in exchange of a permanent token which we need in order to get/gain access on the shopify store
+	$exchange_token_response = $Shopify->exchangeTempTokenForPermanentToken($shop, $code);
+	
+	// validate access token
+	if(!isset($exchange_token_response->access_token) && isset($exchange_token_response->errors)) {
+		// access token is not valid, redirect user to error page
+		echo "<pre>";
+		print_r($exchange_token_response->errors);
+		echo "</pre>";
+	}
+	
+	$access_token = $exchange_token_response->access_token;
+	//echo 'AToken>>'.$access_token;
+	if (empty($access_token)) {
+		echo "Invalid access token";
+	}
+	echo '1211113';
+	// we check if it's a fresh installation
+	$shop_info = $Stores->is_shop_exists($shop);
+	echo '$shop_info>>'.$shop_info;
+	if (empty($shop_info)) {
+		$api_key=SHOPIFY_API_KEY;
+		echo 'fresh installation of app'; // this means that's it's a fresh installation, so we do the installation process
+		$Stores->addData(array(
+				"store_url" => "'$shop'",
+				"access_key" => "'$api_key'",
+				"access_token" => "'$access_token'",
+				"created_at" => "'" . date("Y-m-d") . "'"
+		));
+		
+		
+		// echo "stores>>>>>>".$Stores;
+		
+	} else {  //echo 'inside updateData.............';
+		$Stores->updateData(array(
+				"access_token" => "'$access_token'",
+				"modified_at" => date("Y-m-d")
+		), "store_url = '$shop'");
+	}
+	
+	header("Location: " . APP_URL . "?shop=$shop");
 }
 ?>
 
 <form action="" method="post">
-    <input type="text" name="shop" value="" placeholder="your-store.myshopify.com" />
+    <input type="text" name="shop" value="" placeholder="ex. your-store.myshopify.com" />
     <input type="submit" value="Submit" />
 </form>
