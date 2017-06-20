ee59f88482be0b68de5e19ab52fd698ccc2f796a
priya mehrotra <priya@acmeintech.in>
2017-06-20 17:38:21 +0530

    scan-n-ship

--- app/summary.php
+++ app/summary.php
@@ -5,11 +5,11 @@
  $Stores = new Stores();
  $shop = $_GET['shop'];
  $shop_info = $Stores->is_shop_exists($shop);
- $orders = $Shopify->get_products($shop, $shop_info[0]['token']);
+ $orders = $Shopify->get_orders($shop, $shop_info[0]['token']);
  print_r($orders);
  ?>
 <?php include 'header.php' ?>
 <div id="content">
-<?php include 'order-search.php';  ?>
+
 
 <?php include 'footer.php' ?>
