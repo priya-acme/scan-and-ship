eaedfec6247f9fb2b8ebd13e0efd97c0ff17374c
priya mehrotra <priya@acmeintech.in>
2017-06-21 18:42:26 +0530

    scan-n-ship

--- app/order-search.php
+++ app/order-search.php
@@ -10,17 +10,14 @@ if(isset($_POST['search_order'])){
 	//echo $order_id;
 	$shop_info = $Stores->is_shop_exists($shop);
 	$orders = $Shopify->get_single_order($shop, $shop_info['access_token'],$order_id);
-	echo $order_id;
-	echo $orders->order->id;
-	print_r($orders);
-	if($order_id == $orders['id']){
+	if($order_id == $orders->order->id){
 ?>
-<table>
-<tr>
-<th><b>Order Id</b></th>
-<th><?php echo $orders->id; ?></th>
-</tr>
-</table>
+     <table>
+        <tr>
+          <th><b>Order Id</b></th>
+          <th><?php echo $orders->id; ?></th>
+        </tr>
+     </table>
 <?php 
 	}
 	else{
