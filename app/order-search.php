ad167895f8e9c1741da4415b31a0e212660682f3
priya mehrotra <priya@acmeintech.in>
2017-06-21 18:45:30 +0530

    scan-n-ship

--- app/order-search.php
+++ app/order-search.php
@@ -4,6 +4,8 @@
 <input type="text" name="order_number" id="order_number" />
 <input type="submit" value="Search Order" name="search_order" />
 </form>
+<br>
+<br>
 <?php $shop = $_GET['shop'];
 if(isset($_POST['search_order'])){
 	$order_id = $_POST['order_number'];
@@ -12,16 +14,21 @@ if(isset($_POST['search_order'])){
 	$orders = $Shopify->get_single_order($shop, $shop_info['access_token'],$order_id);
 	if($order_id == $orders->order->id){
 ?>
+     <h2>Order Details</h2>
      <table>
         <tr>
+          <th><b>Order No</b></th>
+          <th><?php echo $orders->order->name; ?></th>
+        </tr>
+         <tr>
           <th><b>Order Id</b></th>
-          <th><?php echo $orders->id; ?></th>
+          <th><?php echo $orders->order->id; ?></th>
         </tr>
      </table>
 <?php 
 	}
 	else{
-		echo "Invalid Id";
+		echo "<h2>Invalid Order Id</h2>";
 	}
 }
 ?>
