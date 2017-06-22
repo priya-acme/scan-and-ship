743c4bb881c833816c5b63a4de068d1a8f2b551f
priya mehrotra <priya@acmeintech.in>
2017-06-22 12:45:08 +0530

    scan-n-ship

--- app/index.php
+++ app/index.php
@@ -1,12 +1,17 @@
-<?php
+<?php session_start();
  include __DIR__ .'../../includes/utils/Shopify.php';
  include __DIR__ .'../../includes/db/Stores.php';
  $Shopify = new Shopify();
  $Stores = new Stores();
  $shop = $_GET['shop'];
- ?>
+ $_SESSION['shop'] = $shop;
+ if($_SESSION['shop']!=null) { ?>
 <?php include 'header.php' ?>
 <div id="content">
 <?php include 'order-search.php';  ?>
 
 <?php include 'footer.php' ?>
+<?php } else {
+	$redirect_url = $Shopify->getAuthUrl($shop);
+	header("Location: $redirect_url");
+}?>
