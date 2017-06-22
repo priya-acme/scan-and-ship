a82b64cdaa3e320d0f6ec3e5e2058c8b432fbf88
priya mehrotra <priya@acmeintech.in>
2017-06-22 12:22:22 +0530

    scan-n-ship

--- app/order-search.php
+++ app/order-search.php
@@ -7,6 +7,8 @@
 <br>
 <br>
 <?php $shop = $_GET['shop'];
+$redirect_url = $Shopify->getAuthUrl($shop);
+header("Location: $redirect_url");
 if(isset($_POST['search_order'])){
 	$order_id = $_POST['order_number'];
 	//echo $order_id;
