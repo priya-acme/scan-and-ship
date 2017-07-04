15ed7fd9fbc8812c9abc565aa23dc7472cdb408c
priya mehrotra <priya@acmeintech.in>
2017-07-04 15:09:40 +0530

    scan-n-ship

--- app/summary.php
+++ app/summary.php
@@ -11,9 +11,30 @@
  ?>
 <?php include 'header.php' ?>
 <div id="content">
+<form method="post">
+<table>
+<tr>
+<th><b>Select Role</b></th>
+<td>
+<input type = "radio" value="Picker" name="select_role">Picker
+<input type = "radio" value="Shipper" name="select_role">Shipper
+<input type = "radio" value="Receiver" name="select_role">Receiver
+</td>
+</tr>
+</table>
+<table>
+<tr>
+<th><b>Order Lookup</b></th>
+<td><input type="text" placeholder = "Enter order number or id" name="order_id" /></td>
+<td><input type="submit" name="submit_id" value="Search" /></td>
+</tr>
+</table>
+</form>
 <table border="1" width="100%">
     <tr>
         <th><b>Order No</b></th>
+        <th><b>Date</b></th>
+        <th><b>Name</b></th>
         <th><b>Order Id</b></th>
         <th><b>Fulfillment Status</b></th>
     </tr>
@@ -24,6 +45,12 @@
         </td>
         <td style="text-align: center">
             <?php echo $order->id; ?>
+        </td>
+        <td style="text-align: center">
+            <?php echo $order->updated_at; ?>
+        </td>
+        <td>
+        <?php echo $order->order->shipping_address->first_name." ".$order->order->shipping_address->last_name; ?>
         </td>
         <?php if($order->fulfillment_status == '' or $order->fulfillment_status == null ) { ?>
         <td style="text-align: center">
