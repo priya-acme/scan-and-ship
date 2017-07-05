dd3ad18c7fe675a9c12a05a4ba80a714cfc6e474
priya mehrotra <priya@acmeintech.in>
2017-07-05 17:31:36 +0530

    scan-n-ship

--- app/header.php
+++ app/header.php
@@ -1,23 +1,33 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Scan And Ship</title>
-<link rel="stylesheet" type="text/css" href="../style.css" media="all" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+  <title>Mobile App</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.css" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 </head>
 <body>
-    <?php $shop =  $_SESSION['shop_name'];?>
-<div id="wrapper">
-<div id="sidebar">
-<div id="menu" class="menu-v">
-  <ul>
-    <li><a href="/scan-and-ship/app/order-search.php/" class="active">Home</a></li>
-	<li><a href="/scan-and-ship/app/summary.php/">Summary Details</a>
-	</li>
-	<li><a href="#">Settings</a>
-  </ul>
+<div class="margtop30">
+<div class="container">
+<div class="row">
+<div class="col-sm-12 col-md-6">
+ <div class="role2">SELECT ROLE</div>
+  <div class="role"><a href="">Picker</a></div>
+   <div class="role"><a href="">Shipper</a></div>
+    <div class="role"><a href="">Receiver</a></div>
 </div>
-<!-- End Menu -->
+<div class="col-sm-12 col-md-6">
+<div class="right-icon">
+<a href="" class="seting-icon">
+<i class="fa fa-cog" aria-hidden="true"></i>
+</a>
+</div>
+</div>
+</div>
+</div>
 </div>
--- app/index.php
+++ app/index.php
@@ -6,41 +6,7 @@ include __DIR__ .'../../includes/utils/Shopify.php';
  $shop = $_GET['shop'];
  $_SESSION['shop_name'] = $shop;
  ?>
- <!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Mobile App</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-  <link rel="stylesheet" href="css/style.css" type="text/css">
-  <link rel="stylesheet" href="font-awesome/css/font-awesome.css" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-</head>
-<body>
-
-<div class="margtop30">
-<div class="container">
-<div class="row">
-<div class="col-sm-12 col-md-6">
- <div class="role2">SELECT ROLE</div>
-  <div class="role"><a href="">Picker</a></div>
-   <div class="role"><a href="">Shipper</a></div>
-    <div class="role"><a href="">Receiver</a></div>
-</div>
-<div class="col-sm-12 col-md-6">
-<div class="right-icon">
-<a href="" class="seting-icon">
-<i class="fa fa-cog" aria-hidden="true"></i>
-</a>
-</div>
-</div>
-</div>
-</div>
-</div>
-
+<?php include 'header.php' ?>
 <div class="margtop30">
 <div class="container">
 <div class="row">
@@ -114,11 +80,7 @@ include __DIR__ .'../../includes/utils/Shopify.php';
 </div>
 </div>
 </div>
-</body>
-</html>
- 
-<?php //include 'header.php' ?>
 <!-- <div id="content"> -->
 <?php //header('location:order-search.php'); ?>
 
-<?php //include 'footer.php' ?>
+<?php include 'footer.php' ?>
