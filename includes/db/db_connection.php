3461cc9a52ff7c750fe8383af87e6097f319ed42
priya mehrotra <priya@acmeintech.in>
2017-07-06 18:55:29 +0530

    scan-n-ship

--- includes/db/db_connection.php
+++ includes/db/db_connection.php
@@ -44,13 +44,8 @@ class DB_Connection{
 		}
 	}
 	function order_verification($sku,$barcode,$order_id,$verification){
-		$query = "insert into order_verification(sku,barcode,order_id,picker_verification) values('$sku','$barcode','$order_id','$verification')";
-		echo $query;
-		$result = mysqli_query($this->connection, $query);
-	}
-	function update_order_verification($sku,$order_id,$verification){
-		$query = "update order_verification set shipper_verification='$verification' where sku='$sku' and order_id='$order_id'";
-		echo $query;
+		$query = "insert into order_verification(sku,barcode,order_id,verification) values('$sku','$barcode','$order_id','$verification')";
+		//echo $query;
 		$result = mysqli_query($this->connection, $query);
 	}
 	function get_order_verification_sku($sku,$order_id){
