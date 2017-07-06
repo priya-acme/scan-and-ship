107b3880aca8d6807616210e4cd65f82ed46ed42
priya mehrotra <priya@acmeintech.in>
2017-07-06 17:54:50 +0530

    scan-n-ship

--- app/order_detailed_page.php
+++ app/order_detailed_page.php
@@ -185,6 +185,7 @@ if(isset($_POST['submit_barcode'])){
         <?php } else { 
         	$get_order_veri_barcode = $Stores->get_order_veri_barcode($variants->variant->barcode, $_REQUEST['id']);
         	$get_order_veri_sku = $Stores->get_order_veri_sku($variants->variant->sku, $_REQUEST['id']);
+        	print_r($get_order_veri_sku);
         	if($get_order_veri_sku['verification']== 'Picker ok' || $get_order_veri_barcode['verification']== 'Picker ok') { ?>
                <td><div class="green"><a href=""><i class="fa fa-check" aria-hidden="true"></i></a></div></td>
         	<?php } else { ?>
