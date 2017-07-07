17482812893cd34d1507a8b51079166a06e1fc33
priya mehrotra <priya@acmeintech.in>
2017-07-07 15:56:23 +0530

    scan-n-ship

--- app/settings.php
+++ app/settings.php
@@ -13,11 +13,11 @@
   <title>Scan and Ship</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
-  <link rel="stylesheet" href="../css/style.css" type="text/css">
-  <link rel="stylesheet" href="../font-awesome/css/font-awesome.css" type="text/css">
+  <link rel="stylesheet" href="../../css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="../../css/style.css" type="text/css">
+  <link rel="stylesheet" href="../../font-awesome/css/font-awesome.css" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="../js/bootstrap.min.js"></script>
+  <script src="../../js/bootstrap.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 </head>
 <body>
