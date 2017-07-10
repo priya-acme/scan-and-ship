0f81c0df0d5da56abaf6e8676bbce50c193d0e0d
priya mehrotra <priya@acmeintech.in>
2017-07-10 15:44:38 +0530

    scan-n-ship

--- app/ajax_call.php
+++ app/ajax_call.php
@@ -5,7 +5,7 @@ $Shopify = new Shopify();
 $Stores = new Stores();
 $shop =  $_SESSION['shop_name'];
 $shop_info = $Stores->is_shop_exists($shop);
-if(empty($Stores->gett_instore_pickup)){
+if(empty($Stores->gett_instore_pickup($_REQUEST['order_id']))){
 $Stores->addd_instore_pickup($_REQUEST['order_id'],$_REQUEST['chkbx_val']);
 echo "ok";
 }
