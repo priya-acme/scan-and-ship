79a6ff9442e6e12c92f326b644bc74500aab9113
priya mehrotra <priya@acmeintech.in>
2017-07-12 18:26:07 +0530

    scan-n-ship

--- app/order-search.php
+++ app/order-search.php
@@ -5,7 +5,7 @@ include __DIR__ .'../../includes/utils/Shopify.php';
  $Stores = new Stores();
  $shop = $_SESSION['shop_name'];
  $shop_info = $Stores->is_shop_exists($shop);
- $orders = $Shopify->get_single_order($shop, $shop_info['access_token'],$_REQUEST['id']);
+ $orders = $Shopify->get_single_order($shop, $shop_info['access_token'],'5814281933');
  //$updateorder = $Shopify->updateOrderInfo($shop, $shop_info['access_token'],'5814281933');
  print_r($orders);
  ?>
