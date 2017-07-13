6d3e065a293406c84e134d9738dc9d53f61857b8
priya mehrotra <priya@acmeintech.in>
2017-07-13 16:25:58 +0530

    scan-n-ship

--- app/picker_ajax_call.php
+++ app/picker_ajax_call.php
@@ -6,9 +6,9 @@ $Stores = new Stores();
 $shop =  $_SESSION['shop_name'];
 $shop_info = $Stores->is_shop_exists($shop);
 $order_id=$_REQUEST['order_id'];
-$check_order_veri = $Stores->check_order_veri($variants->variant->sku, $_REQUEST['id'],$selected_role);
+$check_order_veri = $Stores->check_order_veri($variants->variant->sku, $_REQUEST['id'],$_SESSION['select_role']);
 if(empty($check_order_veri)){
-	$Stores->order_veri($_REQUEST['sku'],"",$order_id,$selected_role);
+	$Stores->order_veri($_REQUEST['sku'],"",$order_id,$_SESSION['select_role']);
 header("location:http://67.207.82.1/scan-and-ship/app/order_detailed_page.php/?id=$order_id");
 }
 ?>
