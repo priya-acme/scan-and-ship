8fd643cd539b465465cf095acda0139a5cae40b7
priya mehrotra <priya@acmeintech.in>
2017-07-13 16:53:32 +0530

    scan-n-ship

--- app/picker_ajax_call.php
+++ app/picker_ajax_call.php
@@ -7,9 +7,11 @@ $shop =  $_SESSION['shop_name'];
 $shop_info = $Stores->is_shop_exists($shop);
 $order_id=$_REQUEST['order_id'];
 $check_order_veri = $Stores->check_order_veri($variants->variant->sku, $_REQUEST['id'],$_REQUEST['role']);
+if($_REQUEST['role']== 'Picker ok' || $_REQUEST['role']== 'Picker' ){
 if(empty($check_order_veri)){
 	$Stores->order_veri($_REQUEST['sku'],"",$order_id,$_REQUEST['role']);
 header("location:http://67.207.82.1/scan-and-ship/app/order_detailed_page.php/?id=$order_id");
+}
 }
 ?>
 
--- app/receiver_ajax_call.php
+++ app/receiver_ajax_call.php
@@ -7,9 +7,11 @@ $shop =  $_SESSION['shop_name'];
 $shop_info = $Stores->is_shop_exists($shop);
 $order_id=$_REQUEST['order_id'];
 $check_order_veri = $Stores->r_check_order_veri($variants->variant->sku, $_REQUEST['id'],$_REQUEST['role']);
+if($_REQUEST['role']== 'Receiver ok' || $_REQUEST['role']== 'Receiver' ){
 if(empty($check_order_veri)){
 	$Stores->r_order_veri($_REQUEST['sku'],"",$order_id,$_REQUEST['role']);
 header("location:http://67.207.82.1/scan-and-ship/app/order_detailed_page.php/?id=$order_id");
+}
 }
 ?>
 
