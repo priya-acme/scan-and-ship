105e3a00597a495f8230241e82ccf7cee60a1cce
priya mehrotra <priya@acmeintech.in>
2017-07-13 18:26:13 +0530

    scan-n-ship

--- includes/db/db_connection.php
+++ includes/db/db_connection.php
@@ -85,7 +85,7 @@ class DB_Connection{
 		$query = "select * from shipper_order_verification where order_id='$order_id' and sku='$sku'";
 		
 		$result = mysqli_query($this->connection, $query);
-		if($result = mysqli_num_rows($result) < 0 ){
+		if(mysqli_num_rows($result) < 0 ){
 	    echo $query;
 		return mysqli_fetch_assoc($result);
 		}
