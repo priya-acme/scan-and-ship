71f82704df13e97248fa38ed3cab653f19f9813d
priya mehrotra <priya@acmeintech.in>
2017-07-17 16:38:01 +0530

    scan-n-ship

--- app/order_test.php
+++ app/order_test.php
@@ -337,12 +337,12 @@ In Store Pickup
         	$get_order_veri_barcode = $Stores->get_order_veri_barcode($variants->variant->barcode, $_REQUEST['id']);
         	$get_order_veri_sku = $Stores->get_order_veri_sku($variants->variant->sku, $_REQUEST['id']);
         	if($get_order_veri_sku['verification']== 'Picker ok' || $get_order_veri_barcode['verification']== 'Picker ok' || $get_order_veri_sku['verification']== 'Picker') {
-        	
-        		if($get_order_veri_sku['quantity'] == $orders->order->line_items[$i]->quantity){ ?>
+        	echo "in if";
+        		if($get_order_veri_sku['quantity'] == $orders->order->line_items[$i]->quantity){ echo "in if qty "; ?>
         <td><div class="green"><a href="" onclick="delete_picker_order('<?php echo $pget_order_id?>','<?php echo $variants->variant->sku ?>')"><i class="fa fa-check" aria-hidden="true"></i></a></div></td>
-    	<?php } else if($get_order_veri_sku['quantity'] != $orders->order->line_items[$i]->quantity && $check_order_veri['quantity'] != 0){ ?>
+    	<?php } else if($get_order_veri_sku['quantity'] != $orders->order->line_items[$i]->quantity && $check_order_veri['quantity'] != 0){ echo "else if"; ?>
     	<td><div class="yellow"><a href=""><i class="fa fa-check" aria-hidden="true"></i></a></div></td>
-    	<?php } else { ?>
+    	<?php } else { echo  "else ";?>
         <td><input type="checkbox" value="<?php echo $variants->variant->sku ?>" onclick="send_picker_value('<?php echo $pget_order_id ?>',this.value,'<?php echo $pselect_role ?>','<?php echo $orders->order->line_items[$i]->quantity?>')" /></td>
         <?php } ?>
         <?php }  } ?>
