b887b94abde527d7678f29d0c7f243b3630152de
priya mehrotra <priya@acmeintech.in>
2017-07-21 14:22:59 +0530

    scan-n-ship

--- app/order_detailed_page.php
+++ app/order_detailed_page.php
@@ -40,7 +40,7 @@ if(isset($_POST['submit_barcode'])){
 				$check_order_veri = $Stores->check_order_veri($variants->variant->sku, $_REQUEST['id'],$selected_role);
 				if(empty($check_order_veri)){
 					$Stores->order_veri($variants->variant->sku,$variants->variant->barcode,$get_order_id,$selected_role,"1");
-					header("location:http://67.207.82.1/scan-and-ship/app/order_detailed_page.php/?id=$get_order_id");
+					//header("location:http://67.207.82.1/scan-and-ship/app/order_detailed_page.php/?id=$get_order_id");
 				}
 				else {
 					//echo $orders->order->line_items[$i]->quantity;
@@ -52,7 +52,7 @@ if(isset($_POST['submit_barcode'])){
 					else {
 						//echo "not equal";
 						$Stores->update_qty_order($variants->variant->sku,$variants->variant->barcode,$get_order_id);
-						header("location:http://67.207.82.1/scan-and-ship/app/order_detailed_page.php/?id=$get_order_id");
+						//header("location:http://67.207.82.1/scan-and-ship/app/order_detailed_page.php/?id=$get_order_id");
 					}
 				}
 			}
