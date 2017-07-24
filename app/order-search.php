a1fa3287021027e1ed8e0ba8b91ce96931e7b725
priya mehrotra <priya@acmeintech.in>
2017-07-24 14:32:08 +0530

    scan-n-ship

--- app/order-search.php
+++ app/order-search.php
@@ -8,7 +8,7 @@ include __DIR__ .'../../includes/utils/Shopify.php';
  $orders = $Shopify->get_single_order($shop, $shop_info['access_token'],'5966458253');
  $updateorder = $Shopify->updateOrderInfo($shop, $shop_info['access_token'],'5966458253',array("order" =>array("note_attributes" => array("In store pickup status"=>"Success"))));
  $create_fulfillment = $Shopify->create_fulfillment_order($shop, $shop_info['access_token'],'5966458253',array("fulfillment"=>array("id"=>"1022782930","order_id"=>"5966458253","status"=>"success","created_at"=>"2017-02-17T11:27:28-05:00",
- 		"service"=>"manual",)));
+ 		"service"=>"manual")));
  echo "<pre>";
  print_r($orders);
  print_r($create_fulfillment);
