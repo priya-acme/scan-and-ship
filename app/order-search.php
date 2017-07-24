6ccc28dc1f808de5b61fb14729caf3e029f9bfeb
priya mehrotra <priya@acmeintech.in>
2017-07-24 13:36:30 +0530

    scan-n-ship

--- app/order-search.php
+++ app/order-search.php
@@ -5,10 +5,10 @@ include __DIR__ .'../../includes/utils/Shopify.php';
  $Stores = new Stores();
  $shop = $_SESSION['shop_name'];
  $shop_info = $Stores->is_shop_exists($shop);
- $orders = $Shopify->get_single_order($shop, $shop_info['access_token'],'5955731725');
- $updateorder = $Shopify->updateOrderInfo($shop, $shop_info['access_token'],'5955731725',array("order" =>array("note_attributes" => array("In store pickup status"=>"Success"))));
+ $orders = $Shopify->get_single_order($shop, $shop_info['access_token'],'5966458253');
+ $updateorder = $Shopify->updateOrderInfo($shop, $shop_info['access_token'],'5966458253',array("order" =>array("note_attributes" => array("In store pickup status"=>"Success"))));
  echo "<pre>";
  print_r($orders);
 //print_r($updateorder);
-echo $orders->order->email;
+//echo $orders->order->email;
  ?>
