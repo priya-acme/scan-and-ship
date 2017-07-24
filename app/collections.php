ee2e82833d3b405af271fc1dd45f8ce3d351a362
priya mehrotra <priya@acmeintech.in>
2017-07-24 17:39:24 +0530

    scan-n-ship

--- app/collections.php
+++ app/collections.php
@@ -5,6 +5,6 @@ $Shopify = new Shopify();
 $Stores = new Stores();
 $shop = $_SESSION['shop_name'];
 $shop_info = $Stores->is_shop_exists($shop);
-$get_collections = $Shopify->get_collections($shop, $shop_info['access_key']);
+$get_collections = $Shopify->get_collections($shop, $shop_info['access_token']);
 print_r($get_collections);
 ?>
