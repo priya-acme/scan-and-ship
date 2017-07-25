23348b4f31faa9659b58172e4397f9aa5c3f59b3
priya mehrotra <priya@acmeintech.in>
2017-07-25 17:47:30 +0530

    scan-n-ship

--- app/collections.php
+++ app/collections.php
@@ -6,9 +6,9 @@ $Stores = new Stores();
 $shop = $_SESSION['shop_name'];
 $shop_info = $Stores->is_shop_exists($shop);
 $get_collections = $Shopify->get_collections($shop, $shop_info['access_token']);
-$modify_col = $Shopify->update_collections($shop, $shop_info['access_token'],array("custom_collections"=>array("collects"=>array("product_id"=>"656607169","position"=>"2"))));
+$modify_col = $Shopify->update_collections($shop, $shop_info['access_token'],array(array("collects"=>array("product_id"=>"656607169","position"=>"2")));
 echo "<pre>";
-print_r($create_collections);
+print_r($modify_col);
 ?>
 <select name="collection">
 <option value="">-select collection-</option>
