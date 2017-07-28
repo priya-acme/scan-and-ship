a17a2b83b89d41bb8b621e2cc9105b9c3aa94a60
priya mehrotra <priya@acmeintech.in>
2017-07-28 11:37:40 +0530

    scan-n-ship

--- app/index.php
+++ app/index.php
@@ -4,10 +4,11 @@ include __DIR__ .'../../includes/utils/Shopify.php';
  $Shopify = new Shopify();
  $Stores = new Stores();
  $shop = $_GET['shop'];
- $_SESSION['shop_name'] = $shop;
+ $_SESSION['shop_name'][] = $shop;
+ echo  $_SESSION['shop_name'][];
  ?>
 <?php include 'header.php' ?>
 <!-- <div id="content"> -->
-<?php header('location:summary_page.php'); ?>
+<?php //header('location:summary_page.php'); ?>
 
 <?php include 'footer.php' ?>
