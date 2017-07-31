5244fe1148e110dcd883bfc387913a1cda9729ae
priya mehrotra <priya@acmeintech.in>
2017-07-31 18:02:00 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -135,16 +135,14 @@ $get_verification = $Stores->get_step_verification($shop);
   <?php for($count=1;$count<=$count_val;$count++){ foreach(${"orders".$count}->orders as $order) {   ?>
   <?php //echo "<pre>";
   //print_r($order);
-  $now = time();
-  
-  $your_date = strtotime($result[0]);
-  //$datediff = $now - $your_date;
-	$input = $order->updated_at; 
+    $now = time();
+    $input = $order->updated_at; 
 	$result = explode('T',$input);
+	$your_date = strtotime($result[0]);
   ?>
   <tr>
     <td width="7%" valign="middle"><strong><a class="order_detail" href="/scan-and-ship/app/order_detailed_page.php/?shop=<?php echo $shop; ?>&&id=<?php echo $order->id; ?>"><?php echo $order->name; ?></a></strong></td>
-    <td width="12%"><strong><?php  echo $result[0]; echo $datediff = $now - $your_date;?></strong></td>
+    <td width="12%"><strong><?php  echo $result[0]; (strtotime($now)- strtotime($your_date))/24/3600; ?></strong></td>
     <td width="12%"><strong><?php echo $order->shipping_address->first_name." ".$order->shipping_address->last_name; ?></strong></td>
     
     <!--  one step verification starts -->
