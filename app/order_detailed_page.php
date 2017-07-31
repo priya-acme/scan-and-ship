d4460085ed3086b84b6208901b14acc33844a379
priya mehrotra <priya@acmeintech.in>
2017-07-31 16:15:56 +0530

    scan-n-ship

--- app/order_detailed_page.php
+++ app/order_detailed_page.php
@@ -676,8 +676,9 @@ if($get_verification['fulfill_order'] == 'On') {
 	$rcount= $Stores->r_count_order($_REQUEST['id']);
 	if(!empty($rcount)){
 		if($rcount['count(*)'] == $sum ){
-			if($orders->order->fulfillment_status == 'fulfilled' ){
-			$create_fulfillment = $Shopify->create_fulfillment_order($shop, $shop_info['access_token'],$_REQUEST['id'],array("fulfillment"=>array("id"=>"","order_id"=>$_REQUEST['id'],"status"=>"success","service"=>"manual")));
+			if($orders->order->fulfillment_status != 'fulfilled' ){
+				echo "hi";
+			//$create_fulfillment = $Shopify->create_fulfillment_order($shop, $shop_info['access_token'],$_REQUEST['id'],array("fulfillment"=>array("id"=>"","order_id"=>$_REQUEST['id'],"status"=>"success","service"=>"manual")));
 			header("location:/scan-and-ship/app/order_detailed_page.php/?shop=$shop&&id=$get_order_id");
 			}
 		}
