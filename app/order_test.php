4e0e8df72ccfe1b95c0a2bec8ff4ae34815498dc
priya mehrotra <priya@acmeintech.in>
2017-07-31 17:24:11 +0530

    scan-n-ship

--- app/order_test.php
+++ app/order_test.php
@@ -1,5 +1,5 @@
 <?php 
 $shop = $_REQUEST['shop'];
 $order_id = $_REQUEST['order_id'];
-header("location:/scan-and-ship/app/order_detailed_page.php?shop=$shop&&id=$order_id");
+header("location:/scan-and-ship/app/order_detailed_page.php/?shop=$shop&&id=$order_id");
 ?>
