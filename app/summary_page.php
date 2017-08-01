adefecb5ceb140b547c826c4632e3aec7b8095db
priya mehrotra <priya@acmeintech.in>
2017-08-01 14:33:49 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -4,9 +4,9 @@
  $Shopify = new Shopify();
  $Stores = new Stores();
  $shop =  $_REQUEST['shop'];
- $code = isset($_GET["code"]) ? $_GET["code"] : false;
- $redirect_url = $Shopify->checkAuthUrl($shop);
- header("Location: $redirect_url");
+//  $code = isset($_GET["code"]) ? $_GET["code"] : false;
+//  $redirect_url = $Shopify->checkAuthUrl($shop);
+//  header("Location: $redirect_url");
  $shop_info = $Stores->is_shop_exists($shop);
  $count_orders = $Shopify->count_orders($shop, $shop_info['access_token']);
  $count_val = ceil($count_orders->count / 250);
