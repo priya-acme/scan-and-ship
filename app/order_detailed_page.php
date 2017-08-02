f8884f68c4ea35de3e96004248119c2e02b5b187
priya mehrotra <priya@acmeintech.in>
2017-08-02 16:55:25 +0530

    scan-n-ship

--- app/order_detailed_page.php
+++ app/order_detailed_page.php
@@ -257,7 +257,7 @@ if(isset($_POST['submit_id']) || isset($_POST['pressed_button']) == 'false'){
 </div>
 </div>
 
-<div class="margtop30">
+<div class="margtop30 ordered-content">
 <div class="container">
 <div class="row">
 <div class="col-md-5 col-sm-12">
