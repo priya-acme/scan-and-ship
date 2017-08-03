7346202367e5f160e7a4c1d72fe8d97b4b1fe2b3
priya mehrotra <priya@acmeintech.in>
2017-08-03 14:58:12 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -22,16 +22,12 @@ $get_verification = $Stores->get_step_verification($shop);
 	$shop_info = $Stores->is_shop_exists($shop);
 	$count_orders = $Shopify->count_orders($shop, $shop_info['access_token']);
 	$count_val = ceil($count_orders->count / 250);
-	
-	
 	for($count=1;$count<=$count_val;$count++){
 	${"get_order".$count} = $Shopify->get_orders($shop,$shop_info['access_token'],$count);
-	echo $count_orders->count;
-	
 	foreach(${"get_order".$count}->orders as $order) {
 		echo ${"get_order".$count};
 		echo $count;
-		die;
+		//die;
 		if($order_id == $order->name || $order_id == $order->id){
 			//header("location:/double-check/app/order_detailed_page.php/?shop=$shop&&id=$order->id");
 		}
