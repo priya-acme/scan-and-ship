60b1e7a1648a8887f01df4d4f99519ed5baab886
priya mehrotra <priya@acmeintech.in>
2017-08-03 15:24:43 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -11,6 +11,7 @@
  $shop_info = $Stores->is_shop_exists($shop);
  $count_orders = $Shopify->count_orders($shop, $shop_info['access_token']);
  $count_val = ceil($count_orders->count / 250);
+ echo $count_val;
  for($count=1;$count<=$count_val;$count++){
  ${"orders".$count} = $Shopify->get_orders($shop, $shop_info['access_token'],$count);
 
