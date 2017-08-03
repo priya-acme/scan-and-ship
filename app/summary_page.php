3388d3bb4062598cd486206e4e5a227c433f053e
priya mehrotra <priya@acmeintech.in>
2017-08-03 15:38:44 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -18,11 +18,12 @@
 $get_verification = $Stores->get_step_verification($shop);
  if(isset($_POST['submit_id'])){
 	$order_id = $_POST['order_id'];
+	echo $_POST['order_id'];
 	$_SESSION['select_role'] = $_POST['select_role'];
 	$shop_info = $Stores->is_shop_exists($shop);
 	
-	$get_single_order = $Shopify->get_single_order($shop,$shop_info['access_token'],$order_id);
-	header("location:/double-check/app/order_detailed_page.php/?shop=$shop&&id=$order_id");
+	//$get_single_order = $Shopify->get_single_order($shop,$shop_info['access_token'],$order_id);
+	//header("location:/double-check/app/order_detailed_page.php/?shop=$shop&&id=$order_id");
 // 	for($count=1;$count<=$count_val;$count++){
 // 	${"get_order".$count} = $Shopify->get_orders($shop,$shop_info['access_token'],$count);
 // 	foreach(${"get_order".$count}->orders as $order) {
