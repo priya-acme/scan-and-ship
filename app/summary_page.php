4559a5229dd5bc8c6610da15f37a2c57f393f5ec
priya mehrotra <priya@acmeintech.in>
2017-08-03 16:38:00 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -17,13 +17,13 @@
  }
 $get_verification = $Stores->get_step_verification($shop);
  if(isset($_POST['submit_id'])){
-	$order_id = $_POST['order_id'];
+	$order_id = $_REQUEST['order_id'];
 	
 	$_SESSION['select_role'] = $_POST['select_role'];
 	$shop_info = $Stores->is_shop_exists($shop);
 	
 	//$get_single_order = $Shopify->get_single_order($shop,$shop_info['access_token'],$order_id);
-	//header("location:/double-check/app/order_detailed_page.php/?shop=$shop&&id=$order_id");
+	header("location:/double-check/app/order_detailed_page.php/?shop=$shop&&id=$order_id");
 // 	for($count=1;$count<=$count_val;$count++){
 // 	${"get_order".$count} = $Shopify->get_orders($shop,$shop_info['access_token'],$count);
 // 	foreach(${"get_order".$count}->orders as $order) {
@@ -37,7 +37,7 @@ $get_verification = $Stores->get_step_verification($shop);
 
 ?>
 <?php include 'header.php' ?>
- <form method="get">
+ <form method="post">
 <div class="margtop30 summary-header-fixed">
 <div class="container">
 <div class="row">
