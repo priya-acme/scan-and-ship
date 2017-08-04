035ee510250ef1c9aea74695706631d785e5e029
priya mehrotra <priya@acmeintech.in>
2017-08-04 18:15:07 +0530

    scan-n-ship

--- app/fulfilled_order.php
+++ app/fulfilled_order.php
@@ -7,5 +7,6 @@ $shop = $_REQUEST['shop'];
 $shop_info = $Stores->is_shop_exists($shop);
 $create_fulfillment = $Shopify->create_fulfillment_order($shop, $shop_info['access_token'],$_REQUEST['order_id'],array("fulfillment"=>array("id"=>"","order_id"=>$_REQUEST['order_id'],"status"=>"success","created_at"=>"2017-02-17T11:27:28-05:00",
 		"service"=>"manual")));
+$updateorder = $Shopify->updateOrderInfo($shop, $shop_info['access_token'],$_REQUEST['order_id'],array("order" =>array("tags" => "Double-Check")));
 //echo "<pre>";
 ?>
