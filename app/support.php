0209bba1fb202276058507f073cfe573d5d508b9
priya mehrotra <priya@acmeintech.in>
2017-08-10 12:07:37 +0530

    scan-n-ship

---
+++ app/support.php
@@ -0,0 +1,43 @@
+<?php
+include __DIR__ .'../../includes/utils/Shopify.php';
+include __DIR__ .'../../includes/db/Stores.php';
+$Shopify = new Shopify();
+$Stores = new Stores();
+$shop = $_REQUEST['shop'];
+$shop_info = $Stores->is_shop_exists($shop);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Double Check - <?php echo $_GET['id']?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="../css/style.css" type="text/css">
+  <link rel="stylesheet" href="../font-awesome/css/font-awesome.css" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+</head>
+<body>
+<div class="margtop30 ordered-header-fixed">
+<div class="container">
+<div class="row">
+<div class="col-sm-12">
+<div class="right-icon">
+<div class="order-btn">
+<a class="order" href="/double-check/app/summary_page.php?shop=<?php echo $shop; ?>">BACK TO ORDER LOOKUP</a>
+</div>
+<a href="/double-check/app/settings.php?shop=<?php echo $shop; ?>" class="seting-icon">
+<i class="fa fa-cog" aria-hidden="true"></i>
+</a>
+<a href="/double-check/app/support.php?shop=<?php echo $shop; ?>" style="text-align:right">Support</a>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+<script type="text/javascript" src="https://form.jotform.me/jsform/72211080736448"></script>
+</body>
