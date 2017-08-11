499121b64746e782c4516663b669b2e399a21a42
priya mehrotra <priya@acmeintech.in>
2017-08-11 18:47:28 +0530

    scan-n-ship

--- app/index.php
+++ app/index.php
@@ -6,47 +6,9 @@ include __DIR__ .'../../includes/utils/Shopify.php';
  $shop = $_GET['shop'];
  $_SESSION[$shop] = $shop;
  //echo $_SESSION[$shop];
- $code = isset($_GET["hmac"]) ? $_GET["hmac"] : false;
- echo $code;
-if ($code) {
- 	// we want to exchange the temp token passed by the shopify server during the installation process
- 	// in exchange of a permanent token which we need in order to get/gain access on the shopify store
- 	$exchange_token_response = $Shopify->exchangeTempTokenForPermanentToken($shop, $code);
- 	
- 	// validate access token
- 	if(!isset($exchange_token_response->access_token) && isset($exchange_token_response->errors)) {
- 		// access token is not valid, redirect user to error page
- 		echo "<pre>";
- 		print_r($exchange_token_response->errors);
- 		echo "</pre>";
- 	}
- 	
- 	$access_token = $exchange_token_response->access_token;
- 	//echo $access_token;
- 	
- 	// we check if it's a fresh installation
- 	$shop_info = $Stores->is_shop_exists($shop);
- 	
- 	if (empty($shop_info)) {
- 		$Stores->addData(array(
- 				"store_url" => $_SESSION[$shop],
- 				"access_key" => SHOPIFY_API_KEY,
- 				"token" => $access_token,
- 				"created_at" => date("Y-m-d")
- 		));
- 	} else {
- 		$Stores->updateData(array(
- 				"access_token" => $access_token,
- 				"access_key" => SHOPIFY_API_KEY,
- 				"created_at" => date("Y-m-d")
- 		), "store_url = '$_SESSION[$shop] '");
- 	}
- 	//echo APP_URL;
- 	header("Location: " . APP_URL."/?shop=$_SESSION[$shop]");
- }
  ?>
-<?php //include 'header.php' ?>
+<?php include 'header.php' ?>
 <!-- <div id="content"> -->
-<?php //header("location:summary_page.php?shop=$_SESSION[$shop]"); ?>
+<?php header("location:summary_page.php?shop=$_SESSION[$shop]"); ?>
 
-<?php //include 'footer.php' ?>
+<?php include 'footer.php' ?>
