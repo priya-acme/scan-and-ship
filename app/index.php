786ca1566927e52692d6789a6eca212bd27c18ce
priya mehrotra <priya@acmeintech.in>
2017-08-14 18:39:27 +0530

    scan-n-ship

--- app/index.php
+++ app/index.php
@@ -56,8 +56,8 @@ include __DIR__ .'../../includes/utils/Shopify.php';
 // $_SESSION[$shop] = $shop;
  //echo $_SESSION[$shop];
  ?>
-<?php //include 'header.php' ?>
+<?php include 'header.php' ?>
 <!-- <div id="content"> -->
-<?php //header("location:summary_page.php?shop=$_SESSION[$shop]"); ?>
+<?php header("location:summary_page.php?shop=$_SESSION[$shop]"); ?>
 
-<?php //include 'footer.php' ?>
+<?php include 'footer.php' ?>
