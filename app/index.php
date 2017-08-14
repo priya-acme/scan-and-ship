69b5675821f0be5b6f143d9f4fa20d0665bd4e06
priya mehrotra <priya@acmeintech.in>
2017-08-14 18:31:25 +0530

    scan-n-ship

--- app/index.php
+++ app/index.php
@@ -4,11 +4,60 @@ include __DIR__ .'../../includes/utils/Shopify.php';
  $Shopify = new Shopify();
  $Stores = new Stores();
  $shop = $_GET['shop'];
- $_SESSION[$shop] = $shop;
+ $code = isset($_GET["code"]) ? $_GET["code"] : false;
+ echo $code;
+ if ($shop && !$code) {
+ 	// validate the shopify url
+ 	if (!$Shopify->validateMyShopifyName($shop)) {
+ 		echo "Invalid shopify url";
+ 	}
+ 	
+ 	$redirect_url = $Shopify->getAuthUrl($shop);
+ 	echo $redirect_url;
+ 	//header("Location: $redirect_url");
+ 	
+ }
+ 
+ if ($code) {
+ 	// we want to exchange the temp token passed by the shopify server during the installation process
+ 	// in exchange of a permanent token which we need in order to get/gain access on the shopify store
+ 	$exchange_token_response = $Shopify->exchangeTempTokenForPermanentToken($shop, $code);
+ 	
+ 	// validate access token
+ 	if(!isset($exchange_token_response->access_token) && isset($exchange_token_response->errors)) {
+ 		// access token is not valid, redirect user to error page
+ 		echo "<pre>";
+ 		print_r($exchange_token_response->errors);
+ 		echo "</pre>";
+ 	}
+ 	
+ 	$access_token = $exchange_token_response->access_token;
+ 	//echo $access_token;
+ 	// we check if it's a fresh installation
+ 	$shop_info = $Stores->is_shop_exists($shop);
+ 	
+//  	if (empty($shop_info)) {
+//  		$Stores->addData(array(
+//  				"store_url" => $shop,
+//  				"access_key" => SHOPIFY_API_KEY,
+//  				"token" => $access_token,
+//  				"created_at" => date("Y-m-d")
+//  		));
+//  	} else {
+//  		$Stores->updateData(array(
+//  				"access_token" => $access_token,
+//  				"access_key" => SHOPIFY_API_KEY,
+//  				"created_at" => date("Y-m-d")
+//  		), "store_url = '$shop'");
+//  	}
+ 	//echo APP_URL;
+ 	//header("Location: " . APP_URL."/?shop=$shop");
+ }
+// $_SESSION[$shop] = $shop;
  //echo $_SESSION[$shop];
  ?>
-<?php include 'header.php' ?>
+<?php //include 'header.php' ?>
 <!-- <div id="content"> -->
-<?php header("location:summary_page.php?shop=$_SESSION[$shop]"); ?>
+<?php //header("location:summary_page.php?shop=$_SESSION[$shop]"); ?>
 
-<?php include 'footer.php' ?>
+<?php //include 'footer.php' ?>
