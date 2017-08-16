300b2fef38535dca04192633d0b3f213f522b044
priya mehrotra <priya@acmeintech.in>
2017-08-16 12:04:33 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -8,6 +8,7 @@ $shop =  $_REQUEST['shop'];
 //  $redirect_url = $Shopify->checkAuthUrl($shop);
 //  //echo $redirect_url;
 //   header("Location: $redirect_url");
+header('location:https://livestock-5.myshopify.com/admin/apps/double-check');
 $shop_info = $Stores->is_shop_exists($shop);
 $count_orders = $Shopify->count_orders($shop, $shop_info['access_token']);
 $count_val = ceil($count_orders->count / 250);
