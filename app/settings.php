fa3ec1e8e2dd280b1e99fb42eda42c09ab95ac44
priya mehrotra <priya@acmeintech.in>
2017-08-18 17:48:32 +0530

    scan-n-ship

--- app/settings.php
+++ app/settings.php
@@ -23,7 +23,7 @@ if(isset($_POST['save_changes'])){
 if(isset($_POST['save_roles'])){
 	$counter = count($_POST["selected_checkbox"]);
 	//print_r($_POST["selected_checkbox"]);
-	for($x=0; $x<$counter; $x++){
+	for($x=1; $x<$counter; $x++){
 		echo $x;
 		if(!empty($_POST["selected_checkbox"][$x])){
 		$store_url = $_POST['store_url'.$x];
