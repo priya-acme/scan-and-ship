d72ca8b5ba75d3f2ae0ab46faf4192cfc0bf396d
priya mehrotra <priya@acmeintech.in>
2017-08-18 17:49:23 +0530

    scan-n-ship

--- app/settings.php
+++ app/settings.php
@@ -125,7 +125,7 @@ if($shop == 'livestock-5.myshopify.com'){
 <td class="hed">STORE URL</td>
 <td class="hed">ASSIGNED ROLES</td>
 </tr>
-<?php $i =0; 
+<?php $i =1; 
 foreach($get_all_stores as $get_stores){ 
 if($get_stores['store_url'] != 'livestock-5.myshopify.com'){ ?>
 <tr>
