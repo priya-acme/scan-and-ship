814c73d312c359c044e4d69b7ce10c069bc26adb
priya mehrotra <priya@acmeintech.in>
2017-08-18 18:38:02 +0530

    scan-n-ship

--- app/settings.php
+++ app/settings.php
@@ -24,14 +24,14 @@ if(isset($_POST['save_roles'])){
 	$counter = count($_POST["selected_checkbox"]);
 	//print_r($_POST["selected_checkbox"]);
 	for($x=0; $x<$counter; $x++){
-		echo $x;
+		//echo $x;
 	
 		$store_url = $_POST['store_url'.$x];
 		$roles =  implode(',', $_POST['selected_role'.$x]);
-		echo $roles;
+		//echo $roles;
 		$saved_stores = $Stores->get_single_save_roles($store_url);
 		$Stores->saved_roles($store_url, $roles);
-       // header("location:/double-check/app/settings.php?shop=$shop");
+        header("location:/double-check/app/settings.php?shop=$shop");
 		
 // 		else {
 // 			$Stores->update_saved_roles($store_url, $roles);
