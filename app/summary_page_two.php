29b277a08a6efe015f759edd50c6f7ee23648853
priya mehrotra <priya@acmeintech.in>
2017-08-22 16:56:12 +0530

    scan-n-ship

--- app/summary_page_two.php
+++ app/summary_page_two.php
@@ -225,7 +225,7 @@ READY FOR PICKUP
     $input = $order->updated_at; 
 	$result = explode('T',$input);
 	$total_days = round(abs(strtotime($now)-strtotime($result[0]))/86400);
-	if($order->fulfillment_status == 'fulfilled'){
+	if($order->fulfillment_status == 'fulfilled' && $order->financial_status != 'refunded' ){
  ?>
   <tr>
     <td width="7%" valign="middle"><strong><a class="order_detail" href="/double-check/app/order_detailed_page.php/?shop=<?php echo $shop; ?>&&id=<?php echo $order->id; ?>"><?php echo $order->name; ?></a></strong></td>
