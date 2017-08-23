1ce69ae2423ac7d2fbd6c28feeed07c134d715ea
priya mehrotra <priya@acmeintech.in>
2017-08-23 15:59:43 +0530

    scan-n-ship

--- app/support.php
+++ app/support.php
@@ -14,6 +14,7 @@ $shop_info = $Stores->is_shop_exists($shop);
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
   <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="stylesheet" href="css/bootstrap-support.css" type="text/css">
   <link rel="stylesheet" href="font-awesome/css/font-awesome.css" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
