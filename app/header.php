1bd554c3add8a6d356e6fd81fd47bdff8bf088ca
priya mehrotra <priya@acmeintech.in>
2017-08-23 14:40:36 +0530

    scan-n-ship

--- app/header.php
+++ app/header.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
   <link rel="stylesheet" href="css/style.css" type="text/css">
   <link rel="stylesheet" href="font-awesome/css/font-awesome.css" type="text/css">
-  <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
   <link href="css/paging.css" rel="stylesheet" type="text/css" />
