706b63e507b6bc55bc4047ec848ae081b5988b41
priya mehrotra <priya@acmeintech.in>
2017-08-30 14:58:40 +0530

    scan-n-ship

--- includes/utils/Shopify.php
+++ includes/utils/Shopify.php
@@ -155,7 +155,7 @@ class Shopify {
 	public function get_fulfilled_orders($shop, $access_token,$count,$date)
 	{
 		$curl_url = "https://$shop/admin/orders.json?limit=250&page=$count&status=any&fulfillment_status=fulfilled";
-		echo $curl_url;
+		//echo $curl_url;
 		return $this->curlRequest($curl_url, $access_token);
 	}
 	 
