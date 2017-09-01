c3a46abb14c5f78524214ec0f9aefdead23243e6
priya mehrotra <priya@acmeintech.in>
2017-09-01 17:52:39 +0530

    scan-n-ship

--- app/summary_page_two.php
+++ app/summary_page_two.php
@@ -160,7 +160,7 @@ READY FOR PICKUP
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="table table-bordered table-responsive mytable" id="table1">
 <thead>
   <tr>
-    <td colspan="3" class="hed" width="35%">ORDER LOOKUP <input type="text" class="txt" name="order_id"> <button type="submit" class="serch" name="submit_id">
+    <td colspan="3" class="hed" width="40%">ORDER LOOKUP <input type="text" class="txt" name="order_id"> <button type="submit" class="serch" name="submit_id">
       <span class="glyphicon glyphicon-search"></span>
     </button></td>
     <?php if($get_verification['verification_step'] == 'One') {  
@@ -214,8 +214,8 @@ READY FOR PICKUP
 	?>
 	<td width="6%" class="hed">PICKED</td>
 	<td width="7%" class="hed">SHIPPED</td>
-	<td width="12%" class="hed">READY FOR PICKUP</td>
-	<td width="11%" class="hed">IN-STORE PICKUP</td>
+	<td width="10%" class="hed">READY FOR PICKUP</td>
+	<td width="10%" class="hed">IN-STORE PICKUP</td>
 	
 	<?php } } ?>
     
