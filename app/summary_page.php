0976e56ee4f1f820aa2408fddede3389e37efe05
priya mehrotra <priya@acmeintech.in>
2017-09-07 14:29:07 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -7,9 +7,9 @@ $ignore_pages   = array('', '');
 
 $dynamic_url    = 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING']; // requested dynamic page (full url)
 $cache_file     = $cache_folder.$cache_ext; // construct a cache file
-$ignore = (in_array($dynamic_url,$ignore_pages))?true:false; //check if url is in ignore list
+// $ignore = (in_array($dynamic_url,$ignore_pages))?true:false; //check if url is in ignore list
 
-if (!$ignore && file_exists($cache_file) && time() - $cache_time < filemtime($cache_file)) { //check Cache exist and it's not expired.
+if (file_exists($cache_file) && time() - $cache_time < filemtime($cache_file)) { //check Cache exist and it's not expired.
     ob_start('ob_gzhandler'); //Turn on output buffering, "ob_gzhandler" for the compressed page with gzip.
     readfile($cache_file); //read Cache file
     echo '<!-- cached page - '.date('l jS \of F Y h:i:s A', filemtime($cache_file)).', Page : '.$dynamic_url.' -->';
