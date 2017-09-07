567ff9a2d7f050bdd5332310d94f979620f2c234
priya mehrotra <priya@acmeintech.in>
2017-09-07 16:27:44 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -5,6 +5,7 @@ $Shopify = new Shopify();
 $Stores = new Stores();
 $shop =  $_REQUEST['shop'];
 $shop_info = $Stores->is_shop_exists($shop);
+
 $thirty_date = date('Y-m-d', strtotime("+30 days"));
 // $date = new DateTime("-6 months");
 // $date->modify("-" . ($date->format('j')-1) . " days");
@@ -60,7 +61,7 @@ $get_single_role = explode(",",$get_single_store['roles']);
 </div>
 <div class="row">
 <div class="col-sm-12 col-md-6">
-<?php if($get_verification['verification_step'] != 'Three') {  ?> <span class="role2 summary-role">SELECT ROLE :</span><?php } ?>
+<?php if($get_verification['verification_step'] != 'Three') {  ?> <span class="role2 summary-role">SELECT ROLE : <?php echo $thirty_date; ?></span><?php } ?>
 <span class="radio radio-primary">
 <?php if($get_verification['verification_step'] == 'One' || $get_verification['verification_step'] == 'Six') {  
 	?>
