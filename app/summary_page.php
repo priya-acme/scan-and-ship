ccbec2238e25de5004cb5f9ba6cdc5cc71948d3f
priya mehrotra <priya@acmeintech.in>
2017-09-07 16:10:01 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -62,7 +62,7 @@ $get_single_role = explode(",",$get_single_store['roles']);
 </div>
 <div class="row">
 <div class="col-sm-12 col-md-6">
-<?php if($get_verification['verification_step'] != 'Three') {  ?> <span class="role2 summary-role">SELECT ROLE :<?php echo $thirty_days?></span><?php } ?>
+<?php if($get_verification['verification_step'] != 'Three') {  ?> <span class="role2 summary-role">SELECT ROLE :<?php echo date('Y-m-d', strtotime("+30 days")); ?></span><?php } ?>
 <span class="radio radio-primary">
 <?php if($get_verification['verification_step'] == 'One' || $get_verification['verification_step'] == 'Six') {  
 	?>
