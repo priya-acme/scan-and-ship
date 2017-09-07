50c3553ffa3fed1c0359b15608adcb8146332b3d
priya mehrotra <priya@acmeintech.in>
2017-09-07 13:14:25 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -1,26 +1,4 @@
 <?php
-//settings
-$cache_ext  = '.html'; //file extension
-$cache_time     = 3600;  //Cache file expires afere these seconds (1 hour = 3600 sec)
-$cache_folder   = '/double-check/app/cached-files/'; //folder to store Cache files
-$ignore_pages   = array('', '');
-
-$dynamic_url    = 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING']; // requested dynamic page (full url)
-$cache_file     = $cache_folder.md5($dynamic_url).$cache_ext; // construct a cache file
-$ignore = (in_array($dynamic_url,$ignore_pages))?true:false; //check if url is in ignore list
-
-if (!$ignore && file_exists($cache_file) && time() - $cache_time < filemtime($cache_file)) { //check Cache exist and it's not expired.
-    ob_start('ob_gzhandler'); //Turn on output buffering, "ob_gzhandler" for the compressed page with gzip.
-    readfile($cache_file); //read Cache file
-    echo '<!-- cached page - '.date('l jS \of F Y h:i:s A', filemtime($cache_file)).', Page : '.$dynamic_url.' -->';
-    ob_end_flush(); //Flush and turn off output buffering
-    exit(); //no need to proceed further, exit the flow.
-}
-//Turn on output buffering with gzip compression.
-ob_start('ob_gzhandler'); 
-######## Your Website Content Starts Below #########
-?>
-<?php
 include __DIR__ .'../../includes/utils/Shopify.php';
 include __DIR__ .'../../includes/db/Stores.php';
 $Shopify = new Shopify();
@@ -81,7 +59,7 @@ $get_single_role = explode(",",$get_single_store['roles']);
 </div>
 <div class="row">
 <div class="col-sm-12 col-md-6">
-<?php if($get_verification['verification_step'] != 'Three') {  ?> <span class="role2 summary-role">SELECT ROLE : <?php echo $cache_file; ?></span><?php } ?>
+<?php if($get_verification['verification_step'] != 'Three') {  ?> <span class="role2 summary-role">SELECT ROLE :</span><?php } ?>
 <span class="radio radio-primary">
 <?php if($get_verification['verification_step'] == 'One' || $get_verification['verification_step'] == 'Six') {  
 	?>
@@ -792,17 +770,3 @@ $(".order_filters a").each(function(){
   })
 </script>
 <?php include 'footer.php' ?>
-<?php
-######## Your Website Content Ends here #########
-
-if (!is_dir($cache_folder)) { //create a new folder if we need to
-    mkdir($cache_folder);
-}
-if(!$ignore){
-    $fp = fopen($cache_file, 'w');  //open file for writing
-    fwrite($fp, ob_get_contents()); //write contents of the output buffer in Cache file
-    fclose($fp); //Close file pointer
-}
-ob_end_flush(); //Flush and turn off output buffering
-
-?>
