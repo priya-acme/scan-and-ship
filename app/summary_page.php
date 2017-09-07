e04385824e24e7534f0dc907445e89a74c7e9c55
priya mehrotra <priya@acmeintech.in>
2017-09-07 14:38:42 +0530

    scan-n-ship

--- app/summary_page.php
+++ app/summary_page.php
@@ -2,11 +2,10 @@
 //settings
 $cache_ext  = '.html'; //file extension
 $cache_time     = 3600;  //Cache file expires afere these seconds (1 hour = 3600 sec)
-$cache_folder   = 'cached-files/'; //folder to store Cache files
 $ignore_pages   = array('', '');
 
 $dynamic_url    = 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$cache_file     = $cache_folder.md5($dynamic_url).$cache_ext; // construct a cache file
+$cache_file     = md5($dynamic_url).$cache_ext; // construct a cache file
 $ignore = (in_array($dynamic_url,$ignore_pages))?true:false; //check if url is in ignore list
 
 if (!$ignore && file_exists($cache_file) && time() - $cache_time < filemtime($cache_file)) { //check Cache exist and it's not expired.
