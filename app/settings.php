8ee3c6d43477d9993d969e0d18871a0164aa4ab1
priya mehrotra <priya@acmeintech.in>
2017-09-08 12:54:35 +0530

    scan-n-ship

--- app/settings.php
+++ app/settings.php
@@ -209,7 +209,7 @@ if(in_array("ready for pickup", $get_single_role)){
 </span>
 </div>
 <div class="col-sm-12 col-md-12">
-<span class="role2"><strong><u>NOTE :</u></strong>"Lowering the number of active days will speed up load times. It is not recommend to go over 15days for Open Orders"</span>
+<span class="role2"><strong><u>NOTE</u>:</strong>&nbsp;&nbsp;"Lowering the number of active days will speed up load times. It is not recommend to go over 15days for Open Orders"</span>
 </div>
 </div>
 
